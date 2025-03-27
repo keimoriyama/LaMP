70b3e1642c9de82aabceb816269470dea3153691
kei <keischwiiz@gmail.com>
2025-03-27 21:25:47 +0900

    rename files

--- LaMP/run_lamp_merge_with_rank.sh
+++
@@ -1,8 +0,0 @@
-DATA=dev
-
-docker compose exec moriyama_task python \
-	LaMP/utils/merge_with_rank.py \
-	--lamp_questions_addr LaMP/${DATA}_questions.json \
-	--lamp_output_addr LaMP/${DATA}_outputs.json \
-	--profile_ranking_addr LaMP/${DATA}_questions_rank.json \
-	--merged_output_addr LaMP/${DATA}_merged_rank.json
