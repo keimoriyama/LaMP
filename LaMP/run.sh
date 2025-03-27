a0199328e7bbbd84b3879f078b1c2a4f63ab0a28
kei <keischwiiz@gmail.com>
2025-03-27 21:22:52 +0900

    update preprocess script

--- LaMP/run.sh
+++ LaMP/run.sh
@@ -1,6 +1,18 @@
+TASK_NAME=LaMP-4
+DATA=dev
+
+
 docker compose exec moriyama_task python \
 	LaMP/rank_profiles.py \
-	--input_data_addr ./LaMP/dev_questions.json \
-	     --task LaMP-4 \
-	     --output_ranking_addr ./LaMP/dev_questions_rank.json \
+	--input_data_addr ./${TASK_NAME}/${DATA}_questions.json \
+	     --task ${TASK_NAME} \
+	     --output_ranking_addr ./outputs/${TASK_NAME}/${DATA}_questions_rank.json \
 	     --ranker contriever
+
+
+docker compose exec moriyama_task python \
+	LaMP/utils/merge_with_rank.py \
+	--lamp_questions_addr ./${TASK_NAME}/${DATA}_questions.json \
+	--lamp_output_addr ./${TASK_NAME}/${DATA}_outputs.json \
+	--profile_ranking_addr ./${TASK_NAME}//${DATA}_questions_rank.json \
+	--merged_output_addr ./${TASK_NAME}/${DATA}_merged_rank.json
