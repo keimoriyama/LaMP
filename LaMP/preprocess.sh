6f8ba7171887c4726999d8e256d45f0b5cc27e7d
keimoriyama <keischwiiz@gmail.com>
2025-03-28 03:58:18 +0000

    update

--- LaMP/preprocess.sh
+++ LaMP/preprocess.sh
@@ -4,15 +4,15 @@ DATA=dev
 
 docker compose exec moriyama_task python \
 	LaMP/rank_profiles.py \
-	--input_data_addr ./${TASK_NAME}/${DATA}_questions.json \
+	--input_data_addr ./LaMP/${TASK_NAME}/${DATA}_questions.json \
 	     --task ${TASK_NAME} \
-	     --output_ranking_addr ./outputs/${TASK_NAME}/${DATA}_questions_rank.json \
+	     --output_ranking_addr ./LaMP/${TASK_NAME}/outputs/${DATA}_questions_rank.json \
 	     --ranker contriever
 
 
 docker compose exec moriyama_task python \
 	LaMP/utils/merge_with_rank.py \
-	--lamp_questions_addr ./${TASK_NAME}/${DATA}_questions.json \
-	--lamp_output_addr ./${TASK_NAME}/${DATA}_outputs.json \
-	--profile_ranking_addr ./${TASK_NAME}//${DATA}_questions_rank.json \
-	--merged_output_addr ./${TASK_NAME}/${DATA}_merged_rank.json
+	--lamp_questions_addr ./LaMP/${TASK_NAME}/${DATA}_questions.json \
+	--lamp_output_addr ./LaMP/${TASK_NAME}/${DATA}_outputs.json \
+	--profile_ranking_addr ./LaMP/${TASK_NAME}/${DATA}_questions_rank.json \
+	--merged_output_addr ./LaMP/${TASK_NAME}/${DATA}_merged_rank.json
