f09473bb062e55f301aa8a7c812268c5add71684
keimoriyama <keischwiiz@gmail.com>
2025-04-04 05:32:41 +0000

    update

--- LaMP/run_train_llm_lamp.sh
+++ LaMP/run_train_llm_lamp.sh
@@ -1,12 +1,10 @@
 TASK_NAME=LaMP-4
 
-python train_llm.py \
+docker compose exec lamp_reproduce python LaMP/train_llm.py \
     --train_data ./LaMP/${TASK_NAME}/train_merged_rank.json \
     --validation_data ./LaMP/${TASK_NAME}/train_merged_rank.json \
     --model_name google/flan-t5-base \
     --task ${TASK_NAME} \
     --output_dir LaMP/results/ \
     --retriever contriver \
-    --use_profile \ /*used to perfrom personalization with RAG */
-    --is_ranked \ /*used if you pre-ranked the profiles based on the provided retrieval model*/
     --num_retrieved 2
